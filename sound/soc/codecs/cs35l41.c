CirrusLogic/linux-drivers

ASoC: cs35l41: DSP Boot control
* Create a control that exposes the halo_booted state variable

* This allows for the DSP to rebooted with different configs

* Set 'DSP Booted' to 0, then set 'DSP1 Preload Switch' to 0
  to shut down the DSP. At this point, the 'DSP1 Firmware'
  control can be changed, and the next time that the Preload
  Switch is enabled or a stream starts, the firmware and
  tuning will be loaded.

SQA-814

Change-Id: I93d587038d365fb107f6f1fed210358f685b8626
Signed-off-by: David Rhodes <david.rhodes@cirrus.com>

sound/soc/codecs/cs35l41.c
@@ -161,6 +161,10 @@ static int cs35l41_dsp_power_ev(struct snd_soc_dapm_widget *w,
 
 		return 0;
 	case SND_SOC_DAPM_PRE_PMD:
+		if (cs35l41->halo_booted == false) {
+			wm_halo_early_event(w, kcontrol, event);
+			wm_halo_event(w, kcontrol, event);
+		}
 	default:
 		return 0;
 	}
@@ -174,9 +178,6 @@ static int cs35l41_dsp_load_ev(struct snd_soc_dapm_widget *w,
 
 	switch (event) {
 	case SND_SOC_DAPM_POST_PMU:
-
-		regmap_write(cs35l41->regmap, CS35L41_CSPL_CAL_STRUCT_ADDR + 4,
-				(unsigned int)CS35L41_AMB_TEMP_DEFAULT);
 		if (cs35l41->halo_booted == false) {
 			wm_halo_event(w, kcontrol, event);
 			cs35l41->halo_booted = true;
@@ -192,6 +193,28 @@ static int cs35l41_dsp_load_ev(struct snd_soc_dapm_widget *w,
 	default:
 		return 0;
 	}
+}
+
+static int cs35l41_halo_booted_get(struct snd_kcontrol *kcontrol,
+			   struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_codec *codec = snd_soc_kcontrol_codec(kcontrol);
+	struct cs35l41_private *cs35l41 = snd_soc_codec_get_drvdata(codec);
+
+	ucontrol->value.integer.value[0] = cs35l41->halo_booted;
+
+	return 0;
+}
+
+static int cs35l41_halo_booted_put(struct snd_kcontrol *kcontrol,
+			   struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_codec *codec = snd_soc_kcontrol_codec(kcontrol);
+	struct cs35l41_private *cs35l41 = snd_soc_codec_get_drvdata(codec);
+
+	cs35l41->halo_booted = ucontrol->value.integer.value[0];
+
+	return 0;
 }
 
 static DECLARE_TLV_DB_SCALE(dig_vol_tlv, -10200, 25, 0);
@@ -255,6 +278,8 @@ static const struct snd_kcontrol_new cs35l41_aud_controls[] = {
 	SOC_SINGLE_TLV("AMP PCM Gain", CS35L41_AMP_GAIN_CTRL, 5, 0x14, 0,
 			amp_gain_tlv),
 	SOC_ENUM("PCM Soft Ramp", pcm_sft_ramp),
+	SOC_SINGLE_EXT("DSP Booted", SND_SOC_NOPM, 0, 1, 0,
+			cs35l41_halo_booted_get, cs35l41_halo_booted_put),
 	WM_ADSP2_PRELOAD_SWITCH("DSP1", 1),
 };
 

chiahsiuchang/linux

sound/soc/codecs/cs35l41.c
@@ -160,6 +160,10 @@ static int cs35l41_dsp_power_ev(struct snd_soc_dapm_widget *w,
 
 		return 0;
 	case SND_SOC_DAPM_PRE_PMD:
+		if (cs35l41->halo_booted == false) {
+			wm_adsp_early_event(w, kcontrol, event);
+			wm_adsp_event(w, kcontrol, event);
+		}
 	default:
 		return 0;
 	}
@@ -173,9 +177,6 @@ static int cs35l41_dsp_load_ev(struct snd_soc_dapm_widget *w,
 
 	switch (event) {
 	case SND_SOC_DAPM_POST_PMU:
-
-		regmap_write(cs35l41->regmap, CS35L41_CSPL_CAL_STRUCT_ADDR + 4,
-				(unsigned int)CS35L41_AMB_TEMP_DEFAULT);
 		if (cs35l41->halo_booted == false) {
 			wm_adsp_event(w, kcontrol, event);
 			cs35l41->halo_booted = true;
@@ -191,6 +192,28 @@ static int cs35l41_dsp_load_ev(struct snd_soc_dapm_widget *w,
 	default:
 		return 0;
 	}
+}
+
+static int cs35l41_halo_booted_get(struct snd_kcontrol *kcontrol,
+			   struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_component *component = snd_soc_kcontrol_component(kcontrol);
+	struct cs35l41_private *cs35l41 = snd_soc_component_get_drvdata(component);
+
+	ucontrol->value.integer.value[0] = cs35l41->halo_booted;
+
+	return 0;
+}
+
+static int cs35l41_halo_booted_put(struct snd_kcontrol *kcontrol,
+			   struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_component *component = snd_soc_kcontrol_component(kcontrol);
+	struct cs35l41_private *cs35l41 = snd_soc_component_get_drvdata(component);
+
+	cs35l41->halo_booted = ucontrol->value.integer.value[0];
+
+	return 0;
 }
 
 static DECLARE_TLV_DB_SCALE(dig_vol_tlv, -10200, 25, 0);
@@ -254,6 +277,8 @@ static const struct snd_kcontrol_new cs35l41_aud_controls[] = {
 	SOC_SINGLE_TLV("AMP PCM Gain", CS35L41_AMP_GAIN_CTRL, 5, 0x14, 0,
 			amp_gain_tlv),
 	SOC_ENUM("PCM Soft Ramp", pcm_sft_ramp),
+	SOC_SINGLE_EXT("DSP Booted", SND_SOC_NOPM, 0, 1, 0,
+			cs35l41_halo_booted_get, cs35l41_halo_booted_put),
 	WM_ADSP2_PRELOAD_SWITCH("DSP1", 1),
 	WM_ADSP_FW_CONTROL("DSP1", 0),
 };